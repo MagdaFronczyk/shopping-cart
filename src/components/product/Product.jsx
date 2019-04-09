4cfafb02e7b5ef1e80577ff42fc3e29816f67cd5
Fronczyk Magdalena <fronczyk@prsa.pl>
2019-04-09 11:48:46 +0200

    prop-types

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+// iport PropTypes from 'prop-types';
 
 const Product = ({ title, thumbnailUrl, id, onClick, text }) => (
   <div>
     <h2>{title}</h2>
     <img src={thumbnailUrl} alt="" />
-    <p>{id}</p>
+    <p>Price: {id}</p>
     <button onClick={onClick}>{text}</button>
   </div>
 )
